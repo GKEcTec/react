9c5ea935b10f2ae74c8f1b043ad55b23063e4136
Hector <hector.alarcon@gk.city>
2025-11-12 10:30:25 -0500

    componentes;

---
+++ 01-reforzamiento/src/bases/11-async-await.ts
@@ -0,0 +1,27 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
+
+const API_KEY = 'Qw2KloqaOA05dvFuBmojaOXJvRBhnTIg';
+
+
+
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+    document.body.append(imgElement)
+}
+
+const getRandomGifUrl = async (): Promise<string> => {
+
+    const response = await fetch(
+        `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    );
+
+    const { data }: GiphyRandomResponse = await response.json();
+
+    return data.images.original.url;
+}
+
+
+// getRandomGifUrl().then((url) => createImageInsideDOM(url))
+getRandomGifUrl().then(createImageInsideDOM)
