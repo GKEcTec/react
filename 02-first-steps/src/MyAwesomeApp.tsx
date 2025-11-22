880ed09ac0643ec6acaac800c1b8a7fe6cba4e1e
GKEcTec <hector.alarcon@gk.city>
2025-11-22 07:45:10 -0500

    css con React

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,16 +1,33 @@
-export const MyAwesomeApp = () => {
-
-    const firstName = 'Héctor';
+import type { CSSProperties } from 'react';
+const firstName = 'Héctor';
     const lastName = 'Alarcón';
 
     const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+    const isActive = false;
 
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'Canadá'
+    }
+    const myStyles: CSSProperties = {
+        backgroundColor:'orange',
+        borderRadius: isActive ? 10 : 20,
+        padding: 10,
+        marginTop: 70,
+    };
+export const MyAwesomeApp = () => {
+                
     return (
         <>
             <h1>{firstName}</h1>
             <h3>{lastName}</h3>
 
             <p>{favoriteGames.join(', ')}</p>
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+            <p style={myStyles}>
+                {JSON.stringify(address)}
+            </p>
         </>
     )
 }
